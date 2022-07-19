3e694ec6269f5884ae40d8f582bfe73ba3095594
Anderson <andersonmatos11@gmail.com>
2022-07-19 09:13:30 -0300

    Add controller layer to Book

---
+++ src/main/kotlin/com/mercadolivro/controller/BookController.kt
@@ -0,0 +1,44 @@
+package com.mercadolivro.controller
+
+import com.mercadolivro.controller.request.PostBookRequest
+import com.mercadolivro.extension.toBookModel
+import com.mercadolivro.model.BookModel
+import com.mercadolivro.service.BookService
+import com.mercadolivro.service.CustomerService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/books")
+class BookController(
+    val bookService: BookService,
+    val customerService: CustomerService
+) {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun create(@RequestBody request: PostBookRequest) {
+        val customer = customerService.getById(request.customerId)
+        bookService.create(request.toBookModel(customer))
+    }
+
+    @GetMapping
+    fun findAll(): List<BookModel> {
+        return bookService.findAll()
+    }
+
+    @GetMapping("/actives")
+    fun findActives(): List<BookModel> {
+        return bookService.findActives()
+    }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Int): BookModel {
+        return bookService.findById(id)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteById(@PathVariable id: Int) {
+        bookService.deleteById(id)
+    }
+}
